9e165db2a2a40814d4e11593049d075c31d30358
sharonrussell <sharon.russell@comparethemarket.com>
2016-07-23 20:04:02 +0100

    Added the structure needed for hands on example one

---
+++ Code/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Code
+{
+    public class StringCalculator
+    {
+        
+    }
+
+    // write a method Add that takes two strings num1 and num2 and returns
+    // the sum of the two numbers
+}
---
+++ Tests/ExampleTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ExampleTests
+    {
+        [TestMethod]
+        public void Should_Pass()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
